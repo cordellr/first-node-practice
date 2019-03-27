3ae654974569321f87607c82cf19299c23d43062
cordellr <cordell.rainock@gmail.com>
2019-03-27 14:39:30 -0500

    Appending data from reddit

--- server/server.js
+++ server/server.js
@@ -8,23 +8,6 @@ request("https://reddit.com/r/popular.json", (err, res, body) => {
   if (err) console.log(err);
 
   JSON.parse(body).data.children.forEach(item => {
-    console.log(item.data.title);
-  });
-
-  fs.writeFile(dataPath, res.body, err => {
-    if (err) console.log(err);
+    fs.appendFileSync(dataPath, item.data.title + "\n");
   });
 });
-
-// fs.readFile(
-//   dataPath,
-//   {
-//     encoding: "UTF-8"
-//   },
-//   (err, data) => {
-//     var person = JSON.parse(data);
-
-//     console.log(person.name);
-//     console.log(person.shirtColor);
-//   }
-// );
